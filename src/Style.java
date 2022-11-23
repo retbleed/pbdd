905e3d35b24e15fd2bd8cef4588bd7a60e849ed9
retbleed <hsa.lapaz@gmail.com>
2022-11-23 10:32:18 -0700

    Todo chill 2

--- src/Style.java
+++ src/Style.java
@@ -1,14 +1,20 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Style extends JComponent {
-    String status;
+
+    private int status;
 
     static final int height = 768;
     static final int width = 1368;
 
-    public Style(){
-
+    public Style(int  status){
+        this.status = status;
     }
 
     public void paintComponent (Graphics g)
@@ -18,6 +24,7 @@ public class Style extends JComponent {
 
         Color statusColorA = new Color(61, 51, 95);
         Color statusColorB = new Color(106, 62, 113);
+        Color statusColorC = new Color(166, 165, 182);
 
         g2.setColor(statusColorA);
         g2.fillRect(0, 0, width, 50);
@@ -26,8 +33,26 @@ public class Style extends JComponent {
         g2.fillRect(0, 50, width, 30);
 
         g2.setColor(statusColorA);
-        g2.fillRect(0, height - 50, width, 50);
+        g2.fillRect(0, height - 50, width, 25);
 
+        switch (status){
+            case 1:
+                g2.setColor(statusColorC);
+                g2.fillRect(235, 100, 1100, 600);
+                break;
+            case 2:
+                g2.setColor(statusColorC);
+                g2.fillRect(10, 100, 1000, 550);
+
+                g2.setColor(statusColorC);
+                g2.fillRect(1050, 100, 265, 150);
+
+                g2.setColor(statusColorC);
+                g2.fillRect(1050, 300, 265, 350);
+                break;
+            default:
+                break;
+            }
         }
 
     }
--- src/Window.java
+++ src/Window.java
@@ -12,11 +12,13 @@ public class Window extends JFrame{
 
     // NO MOVER POR NADA EN EL MUNDO ESTO, LO MUEVES, TE HAGO UN KFC
     private JPanel userScreen;
-    private JTextField userNameInput, userMailInput, userPasswordInput, userInputBalance;
-    private JButton acceptButton, returnButton, setNewBalance, drawBalance;
+    private JTextField userNameInput, userPasswordInput, userInputNewBalance1, userInputNewBalance2, userInputNewBalance3,
+            userInputNewBalance4, userInputNewBalance5, userInputNewBalance6, userInputNewBalance7, userInputNewBalance8,
+            userInputNewBalance9, userInputNewBalance10;
+    private JButton acceptButton, returnButton, logoutButton, addButton, removeButton, searchButton, modifyButton, clientButton, providerButton, productButton;
     private JLabel serverOutput, textA, textB, textC, textD, textE, textF, textG, textH, textI;
-    private Style exam;
 
+    private Style exam;
     static final int height = 768;
     static final int width = 1368;
 
@@ -42,40 +44,154 @@ public class Window extends JFrame{
     public void LoginScreen(){
         userScreen.removeAll();
 
-        textA = new JLabel("SYO - DataBase Manager");
-        textA.setBounds(width/2 - 200 + 50,10,400,30);
-        textA.setFont(new Font("Roboto", Font.PLAIN, 24));
+        textA = new JLabel("DataBase Manager");
+        textA.setBounds(25,10,400,30);
+        textA.setFont(new Font("Monospace", Font.PLAIN, 24));
         textA.setForeground(new Color(253, 184, 52));
         userScreen.add(textA);
 
-        exam = new Style();
+        textB = new JLabel("Nombre de Usuario");
+        textB.setBounds(25,470,400,30);
+        userScreen.add(textB);
+
+        userNameInput = new JTextField();
+        userNameInput.setBounds(20,500,200,24);
+        userScreen.add(userNameInput);
+
+        textC = new JLabel("Contraseña");
+        textC.setBounds(25,520,400,30);
+        userScreen.add(textC);
+
+        userPasswordInput = new JTextField();
+        userPasswordInput.setBounds(20,550,200,24);
+        userScreen.add(userPasswordInput);
+
+        acceptButton = new JButton("Ingresar");
+        acceptButton.setBounds(20,575,200,24);
+
+        //FALTA HACER LA VERIFICACION DE ADMINISTRADOR!!!
+        acceptButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                // TODO Auto-generated method stub
+                    mainScreen();
+                }
+            }
+        );
+        userScreen.add(acceptButton);
+
+        exam = new Style(1);
         exam.setBounds(0,0, width, height);
         userScreen.add(exam);
-
-        this.repaint();
-    }
-
-    public void SignUpScreen (){
-        userScreen.removeAll();
-
         this.repaint();
     }
 
     public void mainScreen(){
         userScreen.removeAll();
 
+        String userName = "Alexander";
 
-        this.repaint();
-    }
+        textA = new JLabel(userName);
+        textA.setBounds(25,10,400,30);
+        textA.setFont(new Font("Monospace", Font.PLAIN, 24));
+        textA.setForeground(new Color(253, 184, 52));
+        userScreen.add(textA);
 
-    public void inventoryScreen(){
-        userScreen.removeAll();
+        for (int i = 0; i < 10; i++){
+            textC = new JLabel("PROBANDO: " + (i * 13));
+            textC.setBounds(35,115 + (i * 13),700,30);
+            userScreen.add(textC);
+        }
 
-        this.repaint();
-    }
+        //  addButton removeButton searchButton modifyButton
 
-    public void sellScreen(){
-        userScreen.removeAll();
+        addButton = new JButton("Añadir");
+        addButton.setBounds(1055, 105, 255,30);
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        userScreen.add(addButton);
+
+        removeButton = new JButton("Eliminar");
+        removeButton.setBounds(1055, 140, 255,30);
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        userScreen.add(removeButton);
+
+        searchButton = new JButton("Buscar");
+        searchButton.setBounds(1055, 175, 255,30);
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        userScreen.add(searchButton);
+
+        modifyButton = new JButton("Modificar");
+        modifyButton.setBounds(1055, 210, 255,30);
+        modifyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        userScreen.add(modifyButton);
+
+        clientButton = new JButton("Cliente");
+        clientButton.setBounds(1055, 310, 255, 75);
+        clientButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        userScreen.add(clientButton);
+
+        providerButton = new JButton("Provedor");
+        providerButton.setBounds(1055, 395, 255, 75);
+        providerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userScreen.remove(textC);
+                repaint();
+            }
+        });
+        userScreen.add(providerButton);
+
+        productButton = new JButton("Producto");
+        productButton.setBounds(1055, 480, 255, 75);
+        productButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        userScreen.add(productButton);
+
+        //SOLO ES PA SALIRSE!
+        logoutButton = new JButton("Cerrar Sesion");
+        logoutButton.setBounds(1080,15,200,24);
+        logoutButton.addActionListener(new ActionListener() {
+                                           @Override
+                                           public void actionPerformed(ActionEvent event) {
+                                               // TODO Auto-generated method stub
+                                               LoginScreen();
+                                           }
+                                       }
+        );
+        userScreen.add(logoutButton);
+
+        exam = new Style(2); //CAMBIAR COLORES SEGUN SEYTU o OMNILIFE
+        exam.setBounds(0,0, width, height);
+        userScreen.add(exam);
 
         this.repaint();
     }
